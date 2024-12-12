f5de707f533565a34edda1d6175a17b789135653
liye480 <202483920010@nuist.edu.cn>
2024-12-12 19:49:28 +0800

    Main

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+
+/**
+ * Program entry
+ */
+public class Main {
+    /**
+     * Entry function
+     */
+    public static void main(String[] args)
+    {
+        //Display function menu1
+        Menu.showMenu();
+    }
+}
